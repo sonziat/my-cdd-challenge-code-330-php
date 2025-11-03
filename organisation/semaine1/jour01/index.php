ac1aa71bd96cdc8dac565618cb52cdab24344790
sonziat <audreysonzia@gmail.com>
2025-11-03 16:50:29 +0100

    ajouts fichiers de travail

---
+++ organisation/semaine1/jour01/index.php
@@ -0,0 +1,8 @@
+<?php
+// Jour 1 - Introduction à PHP
+echo "<h1>Hello PHP !</h1>";
+echo "<p>Bienvenue dans ton apprentissage étape par étape 🚀</p>";
+$nom = "Développeur";
+$date = date("d/m/Y");
+echo "<p>Bonjour $nom, nous sommes le $date.</p>";
+?>
