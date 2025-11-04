1760f3d167d3abdc0f1a8e15af2fd4ca8cf8c163
Sonzia T. Marie-Audrey <audreysonzia@gmail.com>
2025-11-04 23:42:09 +0100

    update day 1

---
+++ organisation/semaine1/jour01/jour01-exercices-affiche-concatene.php
@@ -0,0 +1,30 @@
+<?php
+// Exercice 1 : Écrire "Bonjour le monde"
+echo "Bonjour le monde ";
+
+// Exercice 2 : Afficher votre prénom
+echo "Marie-Audrey"."<br>";
+$prenom="Marie";
+echo $prenom."<br>";
+
+// Exercice 3 : Afficher votre âge
+$age=27;
+echo $age."<br>";
+
+// Exercice 4 : Afficher votre ville
+$ville="Brest";
+echo $ville."<br>";
+// Exercice 5 : Concaténer nom + prénom + ville
+$nom="Sonzia";
+echo $nom." ".$prenom. "J'habite ".$ville."<br>";
+// Exercice 6 : Afficher la date du jour
+$date=date("H:i");
+echo $date;
+
+// Exercice 7 : Utiliser echo pour afficher 3 lignes HTML
+echo "<p>Affiche cette phrase dans un paragraphe</p>"; 
+
+// Exercice 8 : Afficher un message coloré avec HTML + PHP
+echo "<p style="color:bleue">Ceci est un message colore </p>"; 
+
+?>
